3f112af78173a13fa938dd03bafe8314b22cd531
Andrew Vo <andrewqv@uci.edu>
2018-01-21 10:52:33 -0800

    Added photo select

--- FitMate/Controller/ViewControllerPhoto.swift
+++ FitMate/Controller/ViewControllerPhoto.swift
@@ -9,10 +9,11 @@
 import UIKit
 import Firebase
 
-class ViewControllerPhoto: UIViewController {
+class ViewControllerPhoto: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var newUser = userData()
 
+    @IBOutlet weak var myImg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -57,6 +58,34 @@ class ViewControllerPhoto: UIViewController {
         usersInfo.setValue(usersMessage)
         
         self.performSegue(withIdentifier: "goToMainFromPhoto", sender: self)
+    }
+    
+    @IBAction func takePhoto(_ sender: Any) {
+       if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func pickPhoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            myImg.contentMode = .scaleToFill
+            myImg.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
     }
     
     /*
