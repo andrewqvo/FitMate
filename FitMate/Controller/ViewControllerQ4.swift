336c54581954a44d96cdc88864cdcbab1a293267
Derrick Nguyen <derricknguyenn@gmail.com>
2018-01-21 09:00:48 -0800

    Updated exercises

--- FitMate/Controller/ViewControllerQ4.swift
+++ FitMate/Controller/ViewControllerQ4.swift
@@ -10,12 +10,88 @@ import UIKit
 
 class ViewControllerQ4: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var Running: UIButton!
+    @IBOutlet weak var WeightLifting: UIButton!
+    @IBOutlet weak var Dancing: UIButton!
+    @IBOutlet weak var Yoga: UIButton!
+    @IBOutlet weak var Swimming: UIButton!
+    @IBOutlet weak var Cycling: UIButton!
+    
+    
+    @IBAction func RunningAction(_ sender: UIButton){
+        if Running.backgroundColor == UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1){
+            Running.backgroundColor = UIColor.green
+            // add to array
+        }
+        else{
+            Running.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1)
+            // remove from array
+        }
+    }
+    
+    @IBAction func WeightLiftingAction(_ sender: UIButton) {
+        if WeightLifting.backgroundColor == UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1){
+        WeightLifting.backgroundColor = UIColor.green
+        // add to array
+    }
+        else{
+            WeightLifting.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1)
+            // remove from array
+        }
+    }
+    
+    @IBAction func DancingAction(_ sender: UIButton) {
+        if Dancing.backgroundColor == UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1){
+            Dancing.backgroundColor = UIColor.green
+            // add to array
+        }
+        else{
+            Dancing.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1)
+            // remove from array
+        }
+    }
+    
+    @IBAction func YogaAction(_ sender: UIButton) {
+        if Yoga.backgroundColor == UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1){
+            Yoga.backgroundColor = UIColor.green
+            // add to array
+        }
+        else{
+            Yoga.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1)
+            // remove from array
+        }
+    }
+    
+    @IBAction func SwimmingAction(_ sender: UIButton) {
+        if Swimming.backgroundColor == UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1){
+            Swimming.backgroundColor = UIColor.green
+            // add to array
+        }
+        else{
+            Swimming.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1)
+            // remove from array
+        }
+    }
+    
+    @IBAction func CyclingAction(_ sender: UIButton) {
+        if Cycling.backgroundColor == UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1){
+            Cycling.backgroundColor = UIColor.green
+            // add to array
+        }
+        else{
+            Cycling.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1)
+            // remove from array
+        }
+    }
+    
     var newUser = userData()
 
     @IBOutlet weak var favoriteExcercise: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         
         newUser.printAll()
         
