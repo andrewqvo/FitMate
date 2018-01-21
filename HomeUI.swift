c93892560a2c149d17215d265424d6abf6b31410
Derrick Nguyen <derricknguyenn@gmail.com>
2018-01-20 23:20:04 -0800

    Updated ui again

---
+++ HomeUI.swift
@@ -0,0 +1,36 @@
+//
+//  HomeUI.swift
+//  FitMate
+//
+//  Created by Derrick Nguyen on 1/20/18.
+//  Copyright © 2018 Team Alabama. All rights reserved.
+//
+
+import UIKit
+
+class HomeUI: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
