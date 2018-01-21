55ba1b5b1953cc1bf545f5e90a5de956d9d9f38b
Andrew Vo <andrewqv@uci.edu>
2018-01-21 08:55:00 -0800

    Added chat functionality

--- FitMate/Controller/ViewControllerMessage.swift
+++ FitMate/Controller/ViewControllerMessage.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import Firebase
 
-class ViewControllerMessage: UIViewController {
-
+class ViewControllerMessage: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+    
+    var messageArray : [Message] = [Message]()
+    
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var messageTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
+        
+        messageTextField.delegate = self
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tableViewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
+        
+        messageTableView.register(UINib(nibName : "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        
+        condfigureTableView()
+        retrieveMessages()
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +40,82 @@ class ViewControllerMessage: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        
+        cell.messageBody.text = messageArray[indexPath.row].messageBody
+        cell.senderUsername.text = messageArray[indexPath.row].sender
+        cell.avatarImageView.image = UIImage(named: "egg")
+        
+        return cell
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return messageArray.count
+        
+    }
+    
+    @objc func tableViewTapped() {
+        messageTextField.endEditing(true)
+    }
+    
+    func condfigureTableView() {
+        messageTableView.rowHeight = UITableViewAutomaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+    }
+    
+    @IBAction func sendButtonPressed(_ sender: Any) {
+        
+        messageTextField.endEditing(true)
+        
+        //TODO: Send the message to Firebase and save it in our database
+        
+        messageTextField.isEnabled = false
+        sendButton.isEnabled = false
+        
+        let messagesDB = Database.database().reference().child("Messages")
+        
+        let messageDictionary = ["Sender": Auth.auth().currentUser?.email, "MessageBody": messageTextField.text]
+        
+        messagesDB.childByAutoId().setValue(messageDictionary) {
+            (error, reference) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                print("Message saved successfully!")
+                self.messageTextField.isEnabled = true
+                self.sendButton.isEnabled = true
+                self.messageTextField.text = ""
+            }
+        }
+    }
+    
+    func retrieveMessages() {
+        
+        let messageDB = Database.database().reference().child("Messages")
+        
+        messageDB.observe(.childAdded) { (snapshot) in
+            let snapshotValue = snapshot.value as! Dictionary<String,String>
+            
+            let text = snapshotValue["MessageBody"]!
+            let sender = snapshotValue["Sender"]!
+            
+            let message = Message()
+            message.messageBody = text
+            message.sender = sender
+            
+            self.messageArray.append(message)
+            
+            self.condfigureTableView()
+            self.messageTableView.reloadData()
+        }
+        
+    }
+    
+    
+    
 
 }
