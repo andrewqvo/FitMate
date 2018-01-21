94ff8810ec41c072b751524b2e45d92b8a3e3d54
Derrick Nguyen <derricknguyenn@gmail.com>
2018-01-21 09:17:14 -0800

    Updated days

--- FitMate/Controller/ViewControllerQ2.swift
+++ FitMate/Controller/ViewControllerQ2.swift
@@ -13,6 +13,93 @@ class ViewControllerQ2: UIViewController, UITextFieldDelegate {
     var newUser = userData()
 
     @IBOutlet weak var avalibilityInput: UITextField!
+    // BUTTONS
+    
+    @IBOutlet weak var Sunday: UIButton!
+    @IBOutlet weak var Monday: UIButton!
+    @IBOutlet weak var Tuesday: UIButton!
+    @IBOutlet weak var Wednesday: UIButton!
+    @IBOutlet weak var Thursday: UIButton!
+    @IBOutlet weak var Friday: UIButton!
+    @IBOutlet weak var Saturday: UIButton!
+    
+    @IBAction func SundayAction(_ sender: UIButton) {
+        if Sunday.backgroundColor == UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1){
+            Sunday.backgroundColor = UIColor.green
+            // add to array
+        }
+        else{
+            Sunday.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1)
+            // remove from array
+        }
+    }
+    
+    @IBAction func MondayAction(_ sender: UIButton) {
+        if Monday.backgroundColor == UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1){
+            Monday.backgroundColor = UIColor.green
+            // add to array
+        }
+        else{
+            Monday.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1)
+            // remove from array
+        }
+    }
+    
+    @IBAction func Tuesday(_ sender: UIButton) {
+        if Tuesday.backgroundColor == UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1){
+            Tuesday.backgroundColor = UIColor.green
+            // add to array
+        }
+        else{
+            Tuesday.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1)
+            // remove from array
+        }
+    }
+    
+    @IBAction func WednesdayAction(_ sender: UIButton) {
+        if Wednesday.backgroundColor == UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1){
+            Wednesday.backgroundColor = UIColor.green
+            // add to array
+        }
+        else{
+            Wednesday.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1)
+            // remove from array
+        }
+    }
+    
+    @IBAction func ThursdayAction(_ sender: UIButton) {
+        if Thursday.backgroundColor == UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1){
+            Thursday.backgroundColor = UIColor.green
+            // add to array
+        }
+        else{
+            Thursday.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1)
+            // remove from array
+        }
+    }
+    
+    
+    @IBAction func FridayAction(_ sender: UIButton) {
+        if Friday.backgroundColor == UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1){
+            Friday.backgroundColor = UIColor.green
+            // add to array
+        }
+        else{
+            Friday.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1)
+            // remove from array
+        }
+    }
+    
+    @IBAction func SaturdayAction(_ sender: UIButton) {
+        if Saturday.backgroundColor == UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1){
+            Saturday.backgroundColor = UIColor.green
+            // add to array
+        }
+        else{
+            Saturday.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 0/255, alpha: 1)
+            // remove from array
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
